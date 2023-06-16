cb3fa7e2de52991dcaca17d545ec9815226b5bca
coartix <hugodeplagne@gmail.com>
2023-06-16 21:40:01 +0200

    Added ML

--- my-app-local/src/App.tsx
+++ my-app-local/src/App.tsx
@@ -4,11 +4,14 @@ import './App.css';
 import Button from '@mui/material/Button';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import TextField from '@mui/material/TextField';
 
 const App: React.FC = () => {
   const [imageUrls, setImageUrls] = useState<string[]>([]);
   const [prediction, setPrediction] = useState<string>('');
   const [currentImageUrl, setCurrentImageUrl] = useState<string>('');
+  const [text, setText] = useState<string>('');
+  const [annoyance, setAnnoyance] = useState<boolean | null>(null);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files as FileList;
@@ -26,7 +29,7 @@ const App: React.FC = () => {
     }
   };
 
-  const handlePostRequest = async () => {
+  const handlePostRequestCV = async () => {
     if (imageUrls.length > 0) {
       const imageUrl = imageUrls[0];
       setCurrentImageUrl(imageUrl);
@@ -60,6 +63,46 @@ const App: React.FC = () => {
     }
   };
 
+  const handlePostRequestML = async () => {
+    if (text.length > 0) {
+      const data = {
+        "Inputs": {
+          "input1": [
+            {
+              "text": text
+            }
+          ]
+        },
+        "GlobalParameters": {}
+      };
+
+      const body = JSON.stringify(data);
+
+      const url = 'http://d74a7591-5954-4aaf-8624-13e38c7f0304.westeurope.azurecontainer.io/score';
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 1ccaKPtU1D9Bk2wKWQLqoVvjQs8fyEjQ'
+      };
+
+      axios.post(url, body, { headers })
+        .then((response) => {
+          const prediction = response.data
+          let result: number = prediction["Results"]["WebServiceOutput0"][0]
+          if (result === 1) {
+            setAnnoyance(true)
+          }
+          else {
+            setAnnoyance(false)
+          }
+        })
+        .catch((error) => {
+          // Handle the error here
+          console.error(error);
+        });
+      setText("");
+    }
+  };
+
   return (
     <div className='App'>
       <div className='Upload'>
@@ -76,6 +119,7 @@ const App: React.FC = () => {
               <AddIcon /> Upload photo
             </Fab>
           </label>
+          <TextField placeholder='Input text' id="standard-basic" label="Text Heard" variant="standard" value={text} onChange={(e) => setText(e.target.value)} />
         </div>
         <div className='Images'>
           {imageUrls.map((url, index) => (
@@ -87,7 +131,7 @@ const App: React.FC = () => {
       <div className='Buttons'>
         <div className='Column'>
           <div className='Button'>
-            <Button variant="contained" onClick={handlePostRequest} disabled={imageUrls.length === 0}>
+            <Button variant="contained" onClick={handlePostRequestCV} disabled={imageUrls.length === 0}>
               Send request to Custom Vision
             </Button>
           </div>
@@ -102,16 +146,13 @@ const App: React.FC = () => {
         </div>
         <div className='Column'>
           <div className='Button'>
-            <Button variant="contained" onClick={handlePostRequest} disabled={imageUrls.length === 0}>
+            <Button variant="contained" onClick={handlePostRequestML} disabled={text.length === 0}>
               Send request to Azure Machine Learning
             </Button>
           </div>
           <div className='Prediction'>
             <div>
-              {currentImageUrl !== '' && <img src={currentImageUrl} alt="Current" className='Image' />}
-            </div>
-            <div>
-              {prediction}
+              {annoyance != null && (annoyance ? <p>Annoyed</p> : <p>Not annoyed</p>)}
             </div>
           </div>
         </div>
